addfa9a190b662f1655e96aa2cda7251933b5a53
DiegoVivas13 <62918291+DiegoVivas13@users.noreply.github.com>
2020-04-03 23:54:36 -0500

    punto 3

--- preinforme9_diegoVivas_y_DanielCadena.py
+++ preinforme9_diegoVivas_y_DanielCadena.py
@@ -1,11 +1,22 @@
-x = input(("inserte su numero: "))
+x1 = float(input('inserte el valor de la nota 1: '))
+x2 = float(input('inserte el valor de la nota 2: '))
+x3 = float(input('inserte el valor de la nota 3: '))
+x4 = float(input('inserte el valor de la nota 4: '))
+x5 = float(input('inserte el valor de la nota 5: '))
 
-def fninverso (sDato):
-    rta = ""
-    end=len(sDato) - 1
-    while (end >=0):
-        rta = rta + sDato[end]
-        end = end - 1
-    return rta
+n1= x1*0.15
+n2= x2*0.2
+n3= x3*0.15
+n4= x4*0.3
+n5= x5*0.2
+avg = n1 + n2 + n3 + n4 + n5
 
-print ("el numero invertido es: ", fninverso(x))
+if  avg <2 :
+    print ('no puede habilitar', avg)
+elif avg < 3 :
+    print ('reprobó', avg)
+elif avg>=3.0 and avg <= 4.5:
+    print('aprobó', avg)
+elif avg > 4.5 :
+    print ('felicitaciones', avg)
+pass
