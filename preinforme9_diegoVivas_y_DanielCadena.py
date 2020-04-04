0108706b2378ff3ffdbde567c6aa9826e6bcb82c
DiegoVivas13 <62918291+DiegoVivas13@users.noreply.github.com>
2020-04-03 23:52:33 -0500

    punto 2

--- preinforme9_diegoVivas_y_DanielCadena.py
+++ preinforme9_diegoVivas_y_DanielCadena.py
@@ -1,1 +1,11 @@
+x = input(("inserte su numero: "))
 
+def fninverso (sDato):
+    rta = ""
+    end=len(sDato) - 1
+    while (end >=0):
+        rta = rta + sDato[end]
+        end = end - 1
+    return rta
+
+print ("el numero invertido es: ", fninverso(x))
